f25ea520ae68cacd79e3423fd57a1ebfdf393601
Javier Gonzalez <gonzaleztorojavier@gmail.com>
2020-05-04 01:44:13 -0400

    initial gallery component

---
+++ src/components/Gallery/GalleryMain.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class GalleryMain extends Component {
+  render() {
+    return (
+      <div>
+        Galery Div
+      </div>
+    )
+  }
+}
+
+export default GalleryMain
